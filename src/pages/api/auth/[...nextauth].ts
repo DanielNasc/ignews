640809f637a81638404e839b237a1aca86ab5e8f
DanielNasc <danielnasc15987@gmail.com>
2022-07-26 12:46:02 -0300

    create user faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,47 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+
+import { faunaClient } from "../../../services/fuana";
 
 export default NextAuth({
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
-            authorization: {
-                params: {
-                    scope: 'read:user',
-                }
-            }
-        })
-    ]
-})
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+      authorization: {
+        params: {
+          scope: "read:user",
+          secret: process.env.NEXTAUTH_SECRET!,
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        if (!email) {
+          throw new Error("No email found");
+        }
+
+        await faunaClient.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
+      } catch (error) {
+        console.log(error);
+
+        return false;
+      }
+
+      return true;
+    },
+  },
+});
