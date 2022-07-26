7973cd808f5ba58e8284da78a042b9a390e681f4
DanielNasc <danielnasc15987@gmail.com>
2022-07-26 10:05:05 -0300

    github oauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,31 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserSignedIn = true;
+  const { data: session } = useSession();
 
-  return isUserSignedIn ? (
+  return session ? (
     // se o usuário etiver logado, exibe o seu nome do Github e um botão para sair
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="04d361" /> Daniel Nasc
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" /> {session.user?.name}
       <FiX color="737380" className={styles.closeIcon} />
     </button>
   ) : (
     // se o usuário não estiver logado, exibe um botão para fazer login
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="eba417" /> Sign In with Github
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" /> Sign In with Github
     </button>
   );
 }
